0a250ff59271cb484d9881b132f7dbf0196ec148
Indrani890 <137254795+Indrani890@users.noreply.github.com>
2023-08-07 19:02:40 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,12 @@
+##name= input("My name is:");
+##age=input("My age is:");
+##print("My name is:"+name)
+##print("My age is:" +age)
+##print("My age will be 100 in the year of 2083")
+
+
+
+name = input( "What is your name: " )
+age = int(input( "How old are you: " ) )
+year = str( ( 2023 - age ) + 100 )
+print( name + " will be 100 years old in the year " + year )
---
+++ Python/Activities/Activity_10.py
@@ -0,0 +1,6 @@
+tuple=(30,21,50,15,5)
+print(tuple)
+print("Numbers that are divisible by 5:")
+for number in tuple:
+    if(number%5 ==0):
+        print(number)
